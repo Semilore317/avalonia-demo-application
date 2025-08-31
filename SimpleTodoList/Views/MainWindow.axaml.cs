ce835a1b40ca31c940d02d219922930cdd70ecf6
Abraham Oluwasemilore Bankole <semi.banks.317@gmail.com>
2025-08-31 23:32:14 +0100

    fixed delete button bug

--- SimpleTodoList/Views/MainWindow.axaml.cs
+++ SimpleTodoList/Views/MainWindow.axaml.cs
@@ -1,4 +1,5 @@
 using Avalonia.Controls;
+using SimpleTodoList.ViewModels;
 
 namespace SimpleTodoList.Views;
 
@@ -7,5 +8,6 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+        DataContext = new MainWindowViewModel(); // <--- THIS IS REQUIRED
     }
 }
--- SimpleTodoList/obj/Debug/net8.0/SimpleTodoList.AssemblyInfo.cs
+++ SimpleTodoList/obj/Debug/net8.0/SimpleTodoList.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SimpleTodoList")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4d0a82ab8f80019674cde4874f64413cf745275d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e741b624114035c6fb2c7a37bcc2512e2f45e62b")]
 [assembly: System.Reflection.AssemblyProductAttribute("SimpleTodoList")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SimpleTodoList")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

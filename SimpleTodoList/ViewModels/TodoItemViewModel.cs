8b9192ad61721f7ac27b834d3a131cdfa46d15bb
Abraham Oluwasemilore Bankole <semi.banks.317@gmail.com>
2025-08-31 21:55:00 +0100

    scaffolded the TodoItemViewModel

---
+++ SimpleTodoList/ViewModels/TodoItemViewModel.cs
@@ -0,0 +1,35 @@
+using System;
+using CommunityToolkit.Mvvm.ComponentModel;
+using SimpleTodoList.Models;
+
+namespace SimpleTodoList.ViewModels
+{
+    /// <summary>
+    ///  this is a viewModel which represents a <see cref="Models.Todoitem"/>
+    /// </summary>
+    public partial class TodoItemViewModel : ViewModelBase
+    {
+        /// <inheritdoc/>
+
+        public TodoItemViewModel()
+        {
+            // empty
+        }
+        
+        // create a new TodoItemViewModel for the given TodoItem
+        public TodoItemViewModel(TodoItem item)
+        {
+            // init the properties with the given values
+            IsChecked = item.isChecked;
+            Content = item.Content; 
+        }
+        // gets or sets the checked status of each item
+        [ObservableProperty]
+        private bool _isChecked;
+        
+        // gets or sets the content of the to-do item
+        [ObservableProperty]
+        private String? _content;
+        
+    }
+}
